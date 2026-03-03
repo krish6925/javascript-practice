4fe3e1e84cb4a0840b278966cae513d635f108ea
krish6925 <krishbhalotia9876@gmail.com>
2026-03-03 08:09:03 +0000

    dattypes

---
+++ 01_basics/lect.js
@@ -0,0 +1,13 @@
+console.log("krish");
+console.log(3+3);
+let age="33";
+console.log(typeof age);
+
+let n=Number(age);
+console.log(typeof(n));
+let t=6;
+let dt=Boolean(t);
+console.log(dt);
+console.log(typeof undefined);
+console.log(typeof null);
+//""=false else strng =true;
