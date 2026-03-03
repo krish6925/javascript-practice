aa0d62010c9df983caa0e9f098f691c100e57d15
krish6925 <krishbhalotia9876@gmail.com>
2026-03-03 08:25:17 +0000

    comparisons

---
+++ 01_basics/tet.js
@@ -0,0 +1,28 @@
+//+ - * / ** for power 
+console.log(+true);
+console.log(+"");
+let num1,num2,num3;
+num1=num2=num3=2+2;
+console.log(num1);
+console.log(num2);
+console.log(num3);
+let x=10;
+x++;
+console.log(x);
+// also do comparitions
+console.log(2==1);
+console.log(2>1);
+//they will give true or false;
+//what if data type is not same
+//ensure that comparisons compare same data type
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+// first teo gives fals but third gives true;
+console.log(undefined==0);
+console.log(undefined<0);
+console.log(undefined>0);
+// diff between == and ===
+//=== checks the value as well as dat type
+console.log("2"==2);
+console.log("2"===2);
