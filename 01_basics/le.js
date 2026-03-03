5bb58b178f9afef574a53371bd4ff617206890e2
krish6925 <krishbhalotia9876@gmail.com>
2026-03-03 08:55:38 +0000

    datatypes

---
+++ 01_basics/le.js
@@ -0,0 +1,34 @@
+//primitive 
+// 7types: string number boolean niull undefined  bigint symbol
+
+
+//reference data type or non primitive
+// arrays objects functions
+
+//it is a dynamically typed lguage
+//there is nothing as float only numbers eyrything isa in numbers
+
+
+let id=Symbol('123');
+let ih=Symbol('123');
+console.log(id==ih);
+console.log(id===ih)
+const a=9999999999999999999999999999999999n;
+console.log(typeof id);
+console.log(typeof a);
+
+
+///arrays
+
+const heroes=["shanti","krish","dogs"];
+let myobj={
+    name:"krish",
+    age:20
+}
+const my=function(){
+    console.log("hello world");
+}
+
+console.log(typeof heroes)
+console.log(typeof myobj)
+console.log(typeof my);
