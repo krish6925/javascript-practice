0f74cacea4313c049294479f6a8a7dbe722cb5ba
krish6925 <krishbhalotia9876@gmail.com>
2026-03-03 20:31:13 +0000

    arrays

---
+++ 02_basics/arr.js
@@ -0,0 +1,39 @@
+// arrays
+let num=[1,22,3,4,5];
+//it can contain diff datatypes elements
+//it has push and pop and shift and unshift(for inserting elemnt at start)
+//shift(deleting elemt from start)
+
+//includes whether something is present or not gives true or fakse which is boolean
+//index of gives at which index it is present -1 if not present
+const fg=num.join();
+console.log(num);
+console.log(fg);
+console.log(typeof fg);
+// slioce and splice great properties
+
+//slics makes a copy of the subarray
+//splice remove the subarray from the original array
+
+
+//one more major change between slice and splice is
+// in slice the last oindex no. is not rtaken 
+//in splice last index is also taken
+
+
+
+
+
+console.log("a",num);
+const sli=num.slice(1,3);
+console.log("b",num);
+console.log(sli);
+
+const spli=num.splice(1,3);
+console.log("c",num);
+console.log(spli);
+
+
+//;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
