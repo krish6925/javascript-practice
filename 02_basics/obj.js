dc48615e27ed19a48e8786789d409e3f971d4bad
krish6925 <krishbhalotia9876@gmail.com>
2026-03-05 13:51:45 +0000

    objects

---
+++ 02_basics/obj.js
@@ -0,0 +1,77 @@
+// const contai={
+//     name:"krish",
+//     "full name":"krish bhalotia",
+//     age:23,
+//     hobby:"playing",
+//     dob:"24/01/2006"
+// }
+// console.log(contai.name)
+// console.log(contai["name"])
+// console.log(contai["full name"])
+
+// // if there i asymbol and you have to make it a kry in the object its syntax is 
+// //  [write thr name of symbol] is between square gbrackets
+
+// contai.age=98;
+// //Object.freeze(contai.name);
+
+// // object .freeze can freeze the full object or some of its values
+
+
+// contai.age=99;
+// console.log(contai);
+
+// contai.funct=function(){
+//     console.log("helllo");
+// }
+// console.log(contai.funct);
+// //;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+// const myapp=new Object();
+// myapp.name="great";
+// myapp.follow="10k";
+// myapp.height="infinity";
+// console.log(myapp);
+
+// const o1={
+// 1:"a",
+// 2:"b"
+// }
+// const o2={
+//     3:"c",
+//     4:"d"
+// }
+// //Object.assign()//in this write the objects you want to ccombine but remember first one will be the container of all objects
+// // to not disturb original objects we take an empty object first
+// //;like thius object.assign({},o1,o2)
+// //or same as spreading;
+
+// const o3={...o1,...o2};
+// console.log(o3);
+
+// console.log(Object.keys(o1));
+// console.log(Object.values(o1));
+// console.log(Object.entries(o1))
+
+// // o1.hasOwnProperty()is to whether it has a key value or not
+
+// {
+//     "name" :"krish",
+//     "age":23
+
+// }
+
+
+// i hAVE LEFT OBJECT DESTRUCTURING
+
+
+
+
+
+
+
+
+
+
+
+
